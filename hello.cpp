dbbcbe5aa308aeefb73b39883872706c041b9219
Dakota Shearer <dakota.shearer@kysu.edu>
2020-09-15 19:03:13 -0400

    Added Personilzed greeting code

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,10 @@
 
 
 int main() {
+    std::string name;
+    std::cout <<"Enter Your Name " << std::endl;
+    std::cin >>name;
     std::cout << "Hello World." << std::endl;
+    std::cout <<"Hello, " <<name << std::endl;
     return 0;
 }
